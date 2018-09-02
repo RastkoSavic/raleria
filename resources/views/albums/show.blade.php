ea7c8d9b500877b7a1fd6265fddd12418a50db78
RastkoSavic <rastkosavic23@gmail.com>
2018-09-03 00:19:56 +0200

    Display Album Photos

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -7,5 +7,18 @@
 	<a href="/photos/create/{{$album->id}}" class="button">Add Photo</a>
 
 	<hr>
+
+	<div class="grid-x grid-margin-x">
+		@foreach ($album->photos as $photo)
+	
+			<div class="medium-4 cell">
+				<h4>{{$photo->title}}</h4>
+				<a href="/photos/{{$photo->id}}">
+					<img src="/storage/photos/{{$album->id}}/{{$photo->photo}}" alt="{{$photo->title}}" class="thumbnail">
+				</a>
+			</div>
+			
+		@endforeach
+	</div>
 	
 @endsection
